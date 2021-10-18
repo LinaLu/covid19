712cdf0030c964b521c0bc399af02868feaa1a40
LinaLu <lina.my.lu@gmail.com>
2021-10-18 08:54:50 +0200

    modify be response format

--- server/main.py
+++ server/main.py
@@ -14,15 +14,31 @@ covid_risk_model = load_model()
 logger.info("Model loaded.")
 
 
+def risk_context(probability_of_death):
+    return {
+        'Drug poisoning': 0.05,
+        'Covid19': probability_of_death,
+        'Motor vehicle accident': 0.05
+    }
+
+
 @app.route('/api/risk', methods=['POST'])
 def calculate_covid_risk():
     parameters = request.json
     age = parameters.get("age", 0)
+
     probabilities = covid_risk_model.predict_proba([[0, age, 0, 0, 0, 0, 0, 0]])
     probability_of_death = probabilities.tolist()[0][1]
+    probability_of_death = round(probability_of_death, 5)
+    
     return jsonify({
-        'risk': probability_of_death,
-        'parameters': parameters
+        'parameters': parameters,
+        'risk': {
+            'probabilityOfDeath': probability_of_death,
+            'class': 'average',  # low high average
+            'context': risk_context(probability_of_death)
+        },
+
     })
 
 
